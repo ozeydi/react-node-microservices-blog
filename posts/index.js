52dee9ec0050d8cbccd0b3e3290ffcca506ae01c
Zeydi <zeidiwassa@gmail.com>
2021-10-31 11:11:03 +0100

    added posts server

---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const port = 4000;
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id])
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
+
+
+ 
