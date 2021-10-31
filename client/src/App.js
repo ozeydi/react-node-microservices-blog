6a1ba04f4beac6c6e9cd17c22629c71140cd447f
Zeydi <zeidiwassa@gmail.com>
2021-10-31 13:45:26 +0100

    added PostsList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostsList from './PostsList';
 
 const App = () => {
     return <div className="container">
         <h1>Create Post</h1>
-        <PostCreate/>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostsList />
+
     </div>
 }
 
---
+++ client/src/PostsList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PostsList = () => {
+  const [posts, setposts] = useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+    setposts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderPosts = Object.values(posts).map((post) => {
+    return (
+      <div className="card" style={{ width: "30%", marginBottom: "20px"}} key={post.id}>
+          <div className="card-body">
+              <h3>{post.title}</h3>
+          </div>
+      </div>
+    );
+  });
+
+  return <div className="d-flex flew-row flew-wrap justify-content-between">
+      {renderPosts}
+    </div>;
+};
+
+export default PostsList;
