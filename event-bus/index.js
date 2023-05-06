2bed4cef7cc032bcaef860e3c3b20ad7c10db094
Zeydi <zeidiwassa@gmail.com>
2023-05-06 19:39:59 +0200

    added docker files to services

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,49 +1,55 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-
-
-
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const port = 4005;
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
 
 const events = [];
 
-
-app.post('/events', (req, res) => {
-    const event = req.body;
-    events.push(event);
-    console.log('event ' + event.type + ' Received')
-    // posts
-    axios.post('http://localhost:4000/events', event).catch((err) => {
-        console.log(err.message);
-    });
-    //comments
-    axios.post('http://localhost:4001/events', event).catch((err) => {
-        console.log(err.message);
-    });
-    //query
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-        console.log(err.message);
-    });
-    //moderation
-    axios.post("http://localhost:4003/events", event).catch((err) => {
+app.post("/events", (req, res) => {
+  const event = req.body;
+  events.push(event);
+  console.log("event " + event.type + " Received");
+  // posts
+  axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-    
-    res.send({ status: 'OK' });
-})
+  //comments
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  //query
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  //moderation
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // comments server copy1
+  axios.post("http://localhost:4006/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // comments server copy2
+  axios.post("http://localhost:4006/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
-app.get('/events', (req, res) => {
-    res.send(events);
-})
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
+  console.log(`listening on ${port}`);
+});
--- posts/index.js
+++ posts/index.js
@@ -1,45 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-const { randomBytes } = require('crypto');
-
+const { randomBytes } = require("crypto");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
 
 const port = 4000;
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;   
-    posts[id] = {
-        id, title
-    };
-    await axios.post('http://localhost:4005/events', {
-        type: 'PostCreated',
-       data : {id, title}
-    }).catch(err => console.log(err))
-    res.status(201).send(posts[id])
-})
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: { id, title },
+    })
+    .catch((err) => console.log(err));
+  res.status(201).send(posts[id]);
+});
 
-app.post('/events', (req, res) => {
-    console.log('Event Received', req.body.type)
-})
+app.post("/events", (req, res) => {
+  console.log("Event Received", req.body.type);
+});
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
-
-
- 
+  console.log(`listening on ${port}`);
+});
